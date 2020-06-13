cf34901f6868a1436a00fa16e2d6d9349ece04f1
alinikkhah <42302074+alinikkhah1383@users.noreply.github.com>
2020-06-13 22:51:06 +0430

    Change x to integer y

--- first.py
+++ first.py
@@ -1,8 +1,9 @@
 # - *-   coding: utf- 8 - *-
 #hi im starting python2 codding this is a description of my progress
 print("Hi this is an script for find some num is can be / 2 or not ?")
-x = input(int("enter your number :"))
-if x%2==0 :
+x = input("enter your number :")
+y = int(x)
+if y%2==0 :
   print("its can")
 else :
   print("its can't")
