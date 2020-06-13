7bda3a65139567d8399c4f02ebd6bcaa9cc2ff26
alinikkhah <42302074+alinikkhah1383@users.noreply.github.com>
2020-06-13 22:45:09 +0430

    Creat an script

--- first.py
+++ first.py
@@ -1,7 +1,8 @@
 # - *-   coding: utf- 8 - *-
 #hi im starting python2 codding this is a description of my progress
-print("Hi")
-print("this is the first code")
-print("lets learn python together")
-print(":)")
-print("lets connect github")
+print("Hi this is an script for find some num is can be / 2 or not ?")
+x = input(int("enter your number :"))
+if x%2==0 :
+  print("its can")
+else :
+  print("its can't")
