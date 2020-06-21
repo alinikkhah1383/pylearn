6ddfdeeafd4f523c247b6f86f5ddb25d1beb366e
alins.ir <alins.ir@yahoo.com>
2020-06-21 05:28:52 -0700

    add 2 more number

--- first.py
+++ first.py
@@ -1,8 +1,12 @@
 #hi im starting python3 codding this is a description of my progress
-print("Hi this is an script for find some num is can be / 2 or not ?")
+print("Hi this is an script for find some num is can be division by 2 ,3 or 6 or not ?")
 x = input("enter your number :")
 y = int(x)
-if y%2==0 :
-  print("its can")
-else :
-  print("its can't")
+if y%6==0 :
+  print("its can be number 6")
+elif y%3==0 :
+  print("its can be number 3")
+elif y%2==0 :
+  print("it can be number 2")
+else:
+  print("it cant division numbers 2,3 & 6")
