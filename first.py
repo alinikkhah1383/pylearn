a24d4996f6c4790a5cbee92d5ee002a3368b505a
alins.ir <alins.ir@yahoo.com>
2020-06-22 03:57:43 -0700

    add number 5 to script

--- first.py
+++ first.py
@@ -1,5 +1,5 @@
 #hi im starting python3 codding this is a description of my progress
-print("Hi this is an script for find some num is can be division by 2 ,3 or 6 or not ?")
+print("Hi this is an script for find some num is can be division by 2 ,3 ,5 or 6 or not ?")
 x = input("enter your number :")
 y = int(x)
 if y%6==0 :
@@ -8,5 +8,7 @@ elif y%3==0 :
   print("its can be number 3")
 elif y%2==0 :
   print("it can be number 2")
+elif y%5==0 :
+  print("it can by number 5")
 else:
   print("it cant division numbers 2,3 & 6")
