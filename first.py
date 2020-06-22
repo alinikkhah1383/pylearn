987c9af80145cf11e8b1b5ce66618c7e4b691157
alins.ir <alins.ir@yahoo.com>
2020-06-22 03:59:18 -0700

    edit some words

--- first.py
+++ first.py
@@ -3,12 +3,12 @@ print("Hi this is an script for find some num is can be division by 2 ,3 ,5 or 6
 x = input("enter your number :")
 y = int(x)
 if y%6==0 :
-  print("its can be number 6")
+  print("it can be number 6")
 elif y%3==0 :
-  print("its can be number 3")
+  print("it can be number 3")
 elif y%2==0 :
   print("it can be number 2")
 elif y%5==0 :
-  print("it can by number 5")
+  print("it can be number 5")
 else:
-  print("it cant division numbers 2,3 & 6")
+  print("it can't division numbers 2,3 & 6")
