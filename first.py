a266f8cb7b1b8074ae6315f24eac5309f8ceb848
alins.ir <ali1383331@gmail.com>
2020-06-23 11:29:22 -0400

    add number 4 to the script

--- first.py
+++ first.py
@@ -1,5 +1,5 @@
 #hi im starting python3 codding this is a description of my progress
-print("Hi this is an script for find some num is can be division by 2 ,3 ,5 or 6 or not ?")
+print("Hi this is an script for find some num is can be division by 2, 3, 4, 5 or 6 or not ?")
 
 #get number and change from str to float number
 x = input("enter your number :")
@@ -8,13 +8,16 @@ y = float(x)
 #check the number can division
 if y % 6 == 0:
   print("it can be number 6")
+elif y % 5 == 0:
+  print("it can be number 5")
+elif y % 4 == 0:
+  print("it can be number 4")
 elif y % 3 == 0:
   print("it can be number 3")
 elif y % 2 == 0:
   print("it can be number 2")
-elif y % 5 == 0:
-  print("it can be number 5")
 
 #if number can't division then this code start working
 else:
-  print("it can't division numbers 2, 3, 5 & 6")
+  print("it can't division numbers 2, 3, 4, 5 & 6")
+
