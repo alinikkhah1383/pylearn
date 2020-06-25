ce81fdfa0cd36c32a2e38f4a801143cf8a8d07be
alins.ir <ali1383331@gmail.com>
2020-06-25 03:16:09 -0400

    add known numbers to script

--- sqrroot.py
+++ sqrroot.py
@@ -1,1 +1,16 @@
 #an script for find square root of a number
+
+#print some txt about script
+print('this script find square root of any number') 
+
+#get input of number
+number = input('enter your number : ')
+number = float(number)
+
+#square root of two known number
+if (number == 0 or number == 1):
+    squ = number
+
+#print export
+print('square root is : ', squ)
+
